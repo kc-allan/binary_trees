8f8d0aab396ebb73689ab473e8537290eb9805ad
Allan Kirui <allancheruiyot003@gmail.com>
2023-11-06 16:55:24 +0300

    ALX SE

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,12 +6,8 @@
  * @parent: A pointer to the node to insert the left-child in.
  * @value: The value to store in the new node.
  *
- * Return: If parent is NULL or an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
- *
- * Description: If parent already has a left-child, the new node
- *              takes its place and the old left-child is set as
- *              the left-child of the new node.
+ * Return: If parent is NULL or an error occurs - NULL
+ *         Otherwise - a pointer to the new node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
